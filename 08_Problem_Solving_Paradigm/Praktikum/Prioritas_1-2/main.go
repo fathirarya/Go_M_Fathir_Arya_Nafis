5e09420ba436c4b6057481f6f6fe284eedec6e36
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-09-04 13:31:06 +0700

    Submit Task

--- 08_Problem_Solving_Paradigm/Praktikum/Prioritas_1-2/main.go
+++ 08_Problem_Solving_Paradigm/Praktikum/Prioritas_1-2/main.go
@@ -1,49 +1,25 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func generate(numRows int) [][]int {
-	if numRows == 0 {
-		return [][]int{}
-	}
-
-	triangle := make([][]int, numRows)
-	for i := 0; i < numRows; i++ {
-		triangle[i] = make([]int, i+1)
-		triangle[i][0] = 1
-		triangle[i][i] = 1
-
-		for j := 1; j < i; j++ {
-			triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
-		}
-	}
-
-	return triangle
-}
-
-func printPascalTriangle(triangle [][]int) {
-	numRows := len(triangle)
-	maxDigits := len(fmt.Sprint(triangle[numRows-1][numRows-1]))
+func segitigaPascal(numRows int) [][]int {
+	segitiga := [][]int{}
 
 	for i := 0; i < numRows; i++ {
-		padding := maxDigits * (numRows - i - 1)
-		fmt.Printf("%*s", padding/2, "")
+		row := make([]int, i+1)
 
-		for j := 0; j <= i; j++ {
-			fmt.Printf("%-*d", maxDigits, triangle[i][j])
-			if j < i {
-				fmt.Print(" ")
+		for j := 0; j < len(row); j++ {
+			if j == 0 || j == len(row)-1 {
+				row[j] = 1
+			} else {
+				row[j] = segitiga[i-1][j-1] + segitiga[i-1][j]
 			}
 		}
-
-		fmt.Println()
+		segitiga = append(segitiga, row)
 	}
+	return segitiga
 }
 
 func main() {
-	numRows := 5 // Ganti dengan jumlah baris yang diinginkan
-	triangle := generate(numRows)
-	printPascalTriangle(triangle)
+	fmt.Println(segitigaPascal(5))
 }
