7e25d256e56c59115bf935cde46670aacc471534
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-09-30 21:07:45 +0700

    Submit Task

--- 18_Middleware/Praktikum/main.go
+++ 18_Middleware/Praktikum/main.go
@@ -5,12 +5,17 @@ import (
 	"Praktikum/routes"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
 	configs.LoadEnv()
 	configs.InitDB()
+	e.Use(middleware.LoggerWithConfig(
+		middleware.LoggerConfig{
+			Format: "method=${method}, uri=${uri}, status=${status}, time=${time_rfc3339}\n",
+		}))
 	routes.InitUserRoutes(e)
 	routes.InitBookRoutes(e)
 
